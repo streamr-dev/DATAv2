11e83551cb80c41ff303e95b223f8524b27ca0f0
Juuso Takalainen <juuso@streamr.network>
2021-05-27 22:07:48 +0300

    DataTokenMigrator plus test, 100% coverage

---
+++ contracts/DataTokenMigrator.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0;
+
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+contract DataTokenMigrator is Ownable { // is UpgradeAgent, see Crowdsale.sol
+
+    IERC20 public oldToken;
+    IERC20 public newToken;
+
+    function setTokens(IERC20 _oldToken, IERC20 _newToken) public onlyOwner {
+        oldToken = _oldToken;
+        newToken = _newToken;
+    }
+
+    /**
+     * Everything below this comment is the DATA token UpgradeAgent interface.
+     * See https://etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#code
+     */
+
+	// See totalSupply at https://etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
+    uint public originalSupply = 987154514 ether;
+
+    /** UpgradeAgent Interface marker */
+    function isUpgradeAgent() public pure returns (bool) {
+        return true;
+    }
+
+    /**
+     * @param _from token holder that called CrowdsaleToken.upgrade(_value)
+     * @param _value amount of tokens to upgrade, checked by the CrowdsaleToken
+     */
+    function upgradeFrom(address _from, uint256 _value) public {
+        require(msg.sender == address(oldToken), "Call not permitted, UpgradableToken only");
+        newToken.transfer(_from, _value);
+    }
+}
---
+++ test/DataTokenMigrator-test.js
@@ -0,0 +1,68 @@
+const { parseEther } = require("@ethersproject/units")
+const { expect } = require("chai")
+const { ethers } = require("hardhat")
+
+describe("DataTokenMigrator", () => {
+    it("hands out the new tokens in the upgrade", async function() {
+        this.timeout(100000)
+        const [signer, hodler] = await ethers.getSigners()
+
+        // Current DATA token supply
+        // See totalSupply at https://etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
+        const oldSupply = parseEther("987154514")
+
+        // Set up the old token into a valid upgrade state by simulating the crowdsale: hodler gets all, then token is released
+        const CrowdsaleToken = await ethers.getContractFactory("CrowdsaleToken")
+        const oldToken = await CrowdsaleToken.deploy("Streamr DATAcoin", "DATA", 0, 18, true)
+        await oldToken.deployed()
+        await oldToken.setReleaseAgent(signer.address)
+        await oldToken.setMintAgent(signer.address, true)
+        await oldToken.mint(hodler.address, oldSupply)
+        expect(await oldToken.getUpgradeState()).to.equal(1) // NotAllowed
+        await oldToken.releaseTokenTransfer()
+        expect(await oldToken.getUpgradeState()).to.equal(2) // WaitingForAgent
+
+        // WaitingForAgent is also the current pre-migration state of the DATA token in mainnet,
+        // verify that getUpgradeState=2 at https://etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
+
+        // Set the migrator as the upgrade agent in the DATA token contract
+        const DataTokenMigrator = await ethers.getContractFactory("DataTokenMigrator")
+        const migrator = await DataTokenMigrator.deploy()
+        await migrator.deployed()
+        await expect(oldToken.setUpgradeAgent(migrator.address)).to.emit(oldToken, "UpgradeAgentSet")
+        expect(await oldToken.getUpgradeState()).to.equal(3) // ReadyToUpgrade
+
+        // Upgrading still fails because the new token isn't set
+        await expect(oldToken.connect(hodler).upgrade(parseEther("1"))).to.be.reverted
+
+        // Deploy the new token that moves the whole old supply amount to the migrator
+        const DATAv2 = await ethers.getContractFactory("DATAv2")
+        const newToken = await DATAv2.deploy(migrator.address)
+        await newToken.deployed()
+        await migrator.setTokens(oldToken.address, newToken.address)
+
+        // Hodler upgrades some tokens
+        const oldTokensBefore = await oldToken.balanceOf(hodler.address)
+        const newTokensBefore = await newToken.balanceOf(hodler.address)
+        await expect(oldToken.connect(hodler).upgrade(parseEther("1"))).to.emit(oldToken, "Upgrade")
+        await expect(oldToken.connect(hodler).upgrade(parseEther("20"))).to.emit(oldToken, "Upgrade")
+        await expect(oldToken.connect(hodler).upgrade(parseEther("300"))).to.emit(oldToken, "Upgrade")
+        const oldTokensAfter = await oldToken.balanceOf(hodler.address)
+        const newTokensAfter = await newToken.balanceOf(hodler.address)
+
+        // Old token is in "Upgrading" state and new tokens were dispensed correctly
+        expect(await oldToken.getUpgradeState()).to.equal(4) // Upgrading
+        expect(newTokensAfter.sub(newTokensBefore).toString()).to.equal(parseEther("321"))
+        expect(oldTokensBefore.sub(oldTokensAfter).toString()).to.equal(parseEther("321"))
+    })
+
+    it("can't be called by the others than the old token contract", async () => {
+        const [signer] = await ethers.getSigners()
+
+        const DataTokenMigrator = await ethers.getContractFactory("DataTokenMigrator")
+        const migrator = await DataTokenMigrator.deploy()
+        await migrator.deployed()
+
+        await expect(migrator.upgradeFrom(signer.address, parseEther("1"))).to.be.revertedWith("Call not permitted, UpgradableToken only")
+    })
+})
