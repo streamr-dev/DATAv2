f0f9532d0ca5006b353a93bc596f4d283f9560ba
Juuso Takalainen <juuso@streamr.network>
2021-05-27 23:22:31 +0300

    Cleanup, commenting

--- contracts/DATAv2.sol
+++ contracts/DATAv2.sol
@@ -11,9 +11,13 @@ contract DATAv2 is ERC20Permit, ERC20Burnable, AccessControl, IERC677 {
 	string constant NAME = "Streamr DATA v2";
 	string constant SYMBOL = "DATA";
 
+	// ------------------------------------------------------------------------
+	// Immediately mint the initial supply belonging to the old DATA token holders
+	// This makes the DATAv2.totalSupply() correctly reflect the amount of DATA tokens that can be migrated from the old contract
 	// See totalSupply at https://etherscan.io/address/0x0cf0ee63788a0849fe5297f3407f701e122cc023#readContract
 	uint constant INITIAL_SUPPLY = 987154514 ether;
 
+	// ------------------------------------------------------------------------
 	// adapted from @openzeppelin/contracts/token/ERC20/presets/ERC20PresetMinterPauser.sol
 	bytes32 constant MINTER_ROLE = keccak256("MINTER_ROLE");
 
@@ -22,7 +26,6 @@ contract DATAv2 is ERC20Permit, ERC20Burnable, AccessControl, IERC677 {
 	) ERC20(NAME, SYMBOL) ERC20Permit(NAME) {
 		// make contract deployer the role admin that can later grant minter role
         _setupRole(DEFAULT_ADMIN_ROLE, _msgSender());
-
         _mint(migrationContract, INITIAL_SUPPLY);
     }
 
@@ -43,10 +46,9 @@ contract DATAv2 is ERC20Permit, ERC20Burnable, AccessControl, IERC677 {
         _mint(to, amount);
     }
 
+	// ------------------------------------------------------------------------
 	// adapted from LINK token, see https://etherscan.io/address/0x514910771af9ca656af840dff83e8264ecf986ca#code
 	// implements https://github.com/ethereum/EIPs/issues/677
-	// event Transfer(address indexed from, address indexed to, uint value, bytes data);
-
 	/**
 	 * @dev transfer token to a contract address with additional data if the recipient is a contact.
 	 * @param _to The address to transfer to.
