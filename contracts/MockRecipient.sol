bea768a7173050de2a2bb6904c6d93f8bb669336
Juuso Takalainen <juuso@streamr.network>
2023-09-21 13:09:27 +0300

    test: remove console.logging

--- contracts/MockRecipient.sol
+++ contracts/MockRecipient.sol
@@ -1,20 +1,20 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 import "./IERC677Receiver.sol";
 
 contract MockRecipient is IERC677Receiver {
     uint public txCount;
 
     function onTokenTransfer(
-        address _sender,
-        uint256 _value,
+        address, // _sender,
+        uint256, // _value,
         bytes calldata _data
     ) external override {
-        console.log("Received from", _sender);
-        console.log("Amount", _value);
-        console.log("With data", string(_data));
+        // console.log("Received from", _sender);
+        // console.log("Amount", _value);
+        // console.log("With data", string(_data));
         txCount += 1;
         require(keccak256(_data) != keccak256("err")); // for testing: revert if passed "err"
     }
--- contracts/MockRecipientReturnBool.sol
+++ contracts/MockRecipientReturnBool.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.6;
 
-import "hardhat/console.sol";
+// import "hardhat/console.sol";
 
 /**
  * ERC677 recipient that returns false for error instead of reverting
@@ -11,16 +11,16 @@ contract MockRecipientReturnBool {
     uint public txCount;
 
     function onTokenTransfer(
-        address _sender,
-        uint256 _value,
+        address, // _sender,
+        uint256, // _value,
         bytes calldata _data
     ) external returns (bool) {
-        console.log("Received from", _sender);
-        console.log("Amount", _value);
-        console.log("With data", string(_data));
+        // console.log("Received from", _sender);
+        // console.log("Amount", _value);
+        // console.log("With data", string(_data));
         txCount += 1;
         bool retval = keccak256(_data) != keccak256("err"); // for testing: return false if passed "err"
-        console.log("retval", retval);
+        // console.log("retval", retval);
         return retval;
     }
 }
